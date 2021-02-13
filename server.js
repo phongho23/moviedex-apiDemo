e05dafbc02dbf71a110eca3db0ef3a527db3281f
phongho23 <ho.phongt@gmail.com>
2021-02-13 12:16:49 -0500

    added items for heroku deploy

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
-//always place helmet before cors in the pipeline
 const cors = require('cors')
 const MOVIES = require('./movies-data-small.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+
 app.use(helmet())
 app.use(cors())
 
@@ -19,14 +20,12 @@ app.use(function validateBearerToken(req, res, next) {
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized request' })
     }
-    // move to the next middleware
     next()
 })
 
 app.get('/movie', function handleGetMovie(req, res) {
     let response = MOVIES;
 
-    //filter movie by genre if genre query param is present
     if (req.query.genre) {
         response = response.filter(movie =>
             
@@ -36,14 +35,12 @@ app.get('/movie', function handleGetMovie(req, res) {
         )
     }
 
-    // filter by country if param is present
     if (req.query.country) {
         response = response.filter(movie =>
             movie.country.toLowerCase().includes(req.query.country.toLowerCase())
         )
     }
 
-    // filter by query avg_vote if param and number requested
     if (req.query.avg_vote) {
         response = response.filter(movie =>
             Number(movie.avg_vote) >= Number(req.query.avg_vote)
@@ -53,7 +50,17 @@ app.get('/movie', function handleGetMovie(req, res) {
     res.json(response)
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
